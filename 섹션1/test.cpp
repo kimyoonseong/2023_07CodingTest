4c5f61cda05a3ba6a35a7605526fb5818bf2a1df
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2023-10-07 18:46:56 +0900

    split 함수 c++구현 연습

---
+++ 섹션1/test.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+#include <bits/stdc++.h>
+#include<vector>
+using namespace std;
+/*vector<string> split(int input, int delimenter){
+	vector<string> ret;	
+	string token="";
+	long long pos;
+	while((pos=input find(delimiter))!=string::npos){
+		token=input.substr(0,pos);
+		ret.push_back(token);
+		input.erase(0,pos+delimiter.length();	
+		
+	}
+	input.push_back(input);
+	
+}*/
+vector<string> split1(string input, string delimiter){
+	vector<string> ret;
+	long long pos;
+	string token; 
+	while((pos=input.find(delimiter)) != string::npos){
+		token=input.substr(0,pos);
+		ret.push_back(token);
+		input.erase(0, pos+delimiter.length());
+	}
+	ret.push_back(input);
+	return ret;
+}
+int main(){
+	//string a="love is pain";
+	//cout<< a.substr(5,2);
+	//split±â´É
+	string s="b877006±èÀ±¼º °æ±âµµ ¿ëÀÎ½Ã ±âÈï±¸",d=" ";
+	vector<string> a=split1(s,d);
+	for(auto c: a) cout<< c<<"\n";
+}
